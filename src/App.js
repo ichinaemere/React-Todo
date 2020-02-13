608c14bdb2e69d7cc7fda49909ca0b520004534f
Chinaemere Ike <ms.chinaemere@gmail.com>
2020-02-12 20:23:32 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Chinaemere's To-Do List!</h2>
       </div>
     );
   }
