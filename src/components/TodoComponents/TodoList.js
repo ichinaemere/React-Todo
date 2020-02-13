30f0da3698ddd2a565c8a4f259afb79c761f20b0
Chinaemere Ike <ms.chinaemere@gmail.com>
2020-02-13 09:25:50 -0600

    updated my todo list

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,30 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+const myList = [
+{
+    task: 'Organize desk',
+    id: 06071996,
+    completed: false
+}, 
+
+{
+    task: 'Do laundry',
+    id: 09091997,
+    completed: false
+},
+
+{
+    task: 'Meet with Dr. Hammond',
+    id: 01211999,
+    completed: false
+},
+
+{
+    task: 'Clean up room',
+    id: 09212000,
+    completed: false
+}
+
+]
+
